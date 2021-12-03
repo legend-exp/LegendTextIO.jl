80029d26963cc66352bdd0de9b24545991874c31
Oliver Schulz <oschulz@mpp.mpg.de>
2021-12-03 02:41:57 +0100

    Update CI and deps, require Julia v1.6

--- docs/make.jl
+++ docs/make.jl
@@ -1,26 +1,39 @@
 # Use
 #
-#     DOCUMENTER_DEBUG=true julia --color=yes make.jl local [fixdoctests]
+#     DOCUMENTER_DEBUG=true julia --color=yes make.jl local [nonstrict] [fixdoctests]
 #
 # for local builds.
 
 using Documenter
 using LegendTextIO
 
+# Doctest setup
+DocMeta.setdocmeta!(
+    LegendTextIO,
+    :DocTestSetup,
+    :(using LegendTextIO);
+    recursive=true,
+)
+
 makedocs(
     sitename = "LegendTextIO",
     modules = [LegendTextIO],
-    format = :html,
-    pages=[
+    format = Documenter.HTML(
+        prettyurls = !("local" in ARGS),
+        canonical = "https://legend-exp.github.io/LegendTextIO.jl/stable/"
+    ),
+    pages = [
         "Home" => "index.md",
         "API" => "api.md",
         "LICENSE" => "LICENSE.md",
     ],
     doctest = ("fixdoctests" in ARGS) ? :fix : true,
-    html_prettyurls = !("local" in ARGS),
-    html_canonical = "https://legend-exp.github.io/LegendTextIO.jl/stable/",
+    linkcheck = !("nonstrict" in ARGS),
+    strict = !("nonstrict" in ARGS),
 )
 
 deploydocs(
-    repo = "github.com/legend-exp/LegendTextIO.jl.git"
+    repo = "github.com/legend-exp/LegendTextIO.jl.git",
+    forcepush = true,
+    push_preview = true,
 )
--- src/LegendTextIO.jl
+++ src/LegendTextIO.jl
@@ -1,7 +1,5 @@
 # This file is a part of LegendTextIO.jl, licensed under the MIT License (MIT).
 
-__precompile__(true)
-
 module LegendTextIO
 
 using DelimitedFiles
